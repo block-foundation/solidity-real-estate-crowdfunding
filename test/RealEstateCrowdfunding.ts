7a7d3baa3d892d61da0733d791d0b8417f4b550e
Lars van Vianen <lars@vianen.com>
2023-08-16 14:39:52 +0200

    Update RealEstateCrowdfunding.ts

--- test/RealEstateCrowdfunding.ts
+++ test/RealEstateCrowdfunding.ts
@@ -19,94 +19,129 @@ import chai from "chai";
 import { solidity } from "ethereum-waffle";
 import { RealEstateCrowdfunding } from "../typechain/RealEstateCrowdfunding";
 
+
 // Set up Waffle with Chai
 chai.use(solidity);
 const { expect } = chai;
 
 // Main contract test block
 describe("Real Estate Crowdfunding", () => {
-  // Declare contract variable in the test scope
-  let contract: RealEstateCrowdfunding;
+    // Declare contract variable in the test scope
+    let contract: RealEstateCrowdfunding;
 
-  // Before running each test, deploy the contract
-  beforeEach(async () => {
-    const signers = await ethers.getSigners();
-    const contractFactory = await ethers.getContractFactory("RealEstateCrowdfunding", signers[0]);
-    contract = (await contractFactory.deploy()) as RealEstateCrowdfunding;
-    await contract.deployed();
-  });
+    // Before running each test, deploy the contract
+    beforeEach(
+        async () => {
+            const signers = await ethers.getSigners();
+            const contractFactory = await ethers.getContractFactory(
+                "RealEstateCrowdfunding",
+                signers[0]
+            );
+            contract = (await contractFactory.deploy()) as RealEstateCrowdfunding;
+            await contract.deployed();
+        }
+    );
 
-  // Test case for campaign creation
-  it("should allow a user to create a campaign", async () => {
-    await contract.createCampaign(ethers.utils.parseEther("10"));
-    const campaign = await contract.campaigns(0);
+    // Test case for campaign creation
+    it(
+        "should allow a user to create a campaign",
+        async () => {
+            await contract.createCampaign(ethers.utils.parseEther("10"));
+            const campaign = await contract.campaigns(0);
 
-    // Verify that the beneficiary and goal are correctly set
-    expect(campaign.beneficiary).to.equal(await contract.signer.getAddress());
-    expect(campaign.goal).to.equal(ethers.utils.parseEther("10"));
-  });
+            // Verify that the beneficiary and goal are correctly set
+            expect(campaign.beneficiary).to.equal(await contract.signer.getAddress());
+            expect(campaign.goal).to.equal(ethers.utils.parseEther("10"));
+        }
+    );
 
-  // Test case for contributing to a campaign
-  it("should allow a user to contribute to a campaign", async () => {
-    await contract.createCampaign(ethers.utils.parseEther("10"));
+    // Test case for contributing to a campaign
+    it(
+        "should allow a user to contribute to a campaign",
+        async () => {
+            await contract.createCampaign(ethers.utils.parseEther("10"));
 
-    const contributionAmount = ethers.utils.parseEther("5");
-    await contract.contribute(0, { value: contributionAmount });
+            const contributionAmount = ethers.utils.parseEther("5");
+            await contract.contribute(0, { value: contributionAmount });
 
-    const contribution = await contract.checkContribution(0);
+            const contribution = await contract.checkContribution(0);
 
-    // Verify that the contribution was correctly registered
-    expect(contribution).to.equal(contributionAmount);
-  });
+            // Verify that the contribution was correctly registered
+            expect(contribution).to.equal(contributionAmount);
+        }
+    );
 
-  // Test case to prevent overfunding
-  it("should not allow a user to contribute more than the goal", async () => {
-    await contract.createCampaign(ethers.utils.parseEther("10"));
+    // Test case to prevent overfunding
+    it(
+        "should not allow a user to contribute more than the goal",
+        async () => {
+            await contract.createCampaign(ethers.utils.parseEther("10"));
 
-    // Attempt to overfund and expect a revert
-    await expect(
-        contract.contribute(0, { value: ethers.utils.parseEther("15") })
-    ).to.be.revertedWith("Campaign already funded");
-  });
+            // Attempt to overfund and expect a revert
+            await expect(
+                contract.contribute(0, { value: ethers.utils.parseEther("15") })
+            ).to.be.revertedWith("Campaign already funded");
+        }
+    );
 
-  // Test case for the withdrawal of funds when the goal is met
-  it("should allow the beneficiary to withdraw funds when the goal is met", async () => {
-    await contract.createCampaign(ethers.utils.parseEther("10"));
-    await contract.contribute(0, { value: ethers.utils.parseEther("10") });
+    // Test case for the withdrawal of funds when the goal is met
+    it(
+        "should allow the beneficiary to withdraw funds when the goal is met",
+        async () => {
+            await contract.createCampaign(ethers.utils.parseEther("10"));
+            await contract.contribute(
+                0,
+                { value: ethers.utils.parseEther("10") }
+            );
 
-    await contract.withdrawFunds(0);
+            await contract.withdrawFunds(0);
 
-    const campaign = await contract.campaigns(0);
+            const campaign = await contract.campaigns(0);
 
-    // Verify that funds were correctly withdrawn
-    expect(campaign.raisedAmount).to.equal(0);
-  });
+            // Verify that funds were correctly withdrawn
+            expect(campaign.raisedAmount).to.equal(0);
+        }
+    );
 
-  // Test case to prevent premature withdrawal
-  it("should not allow the beneficiary to withdraw funds before the goal is met", async () => {
-    await contract.createCampaign(ethers.utils.parseEther("10"));
-    await contract.contribute(0, { value: ethers.utils.parseEther("5") });
+    // Test case to prevent premature withdrawal
+    it(
+        "should not allow the beneficiary to withdraw funds before the goal is met",
+        async () => {
+            await contract.createCampaign(ethers.utils.parseEther("10"));
+            await contract.contribute(
+                0,
+                { value: ethers.utils.parseEther("5") }
+            );
 
-    // Attempt premature withdrawal and expect a revert
-    await expect(
-        contract.withdrawFunds(0)
-    ).to.be.revertedWith("Fundraising goal not met");
-  });
+            // Attempt premature withdrawal and expect a revert
+            await expect(
+                contract.withdrawFunds(0)
+            ).to.be.revertedWith("Fundraising goal not met");
+        }
+    );
 
-  // Test case to refund the contribution if the goal is not met
-  it("should allow a user to refund their contribution if the goal is not met", async () => {
-    await contract.createCampaign(ethers.utils.parseEther("10"));
-    await contract.contribute(0, { value: ethers.utils.parseEther("5") });
+    // Test case to refund the contribution if the goal is not met
+    it(
+        "should allow a user to refund their contribution if the goal is not met",
+        async () => {
+            await contract.createCampaign(ethers.utils.parseEther("10"));
+            await contract.contribute(
+                0,
+                { value: ethers.utils.parseEther("5") }
+            );
 
-    await ethers.provider.send("evm_increaseTime", [86400 * 7]); // simulate time passing
-    await ethers.provider.send("evm_mine", []); // mine the next block
+            // simulate time passing
+            await ethers.provider.send("evm_increaseTime", [86400 * 7]);
+            // mine the next block
+            await ethers.provider.send("evm_mine", []);
 
-    await contract.refund(0);
+            await contract.refund(0);
 
-    const contribution = await contract.checkContribution(0);
+            const contribution = await contract.checkContribution(0);
 
-    // Verify that funds were correctly refunded
-    expect(contribution).to.equal(0);
-  });
+            // Verify that funds were correctly refunded
+            expect(contribution).to.equal(0);
+        }
+    );
 
 });
